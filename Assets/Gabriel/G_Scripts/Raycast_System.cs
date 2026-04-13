0bcee1f2a0d60dd53cd95db7bf555a91bfc8eb2e
noyratv <noyratv@gmail.com>
2026-04-13 10:29:46 +0200

    WIP Raycast System

--- Assets/Gabriel/G_Scripts/Raycast_System.cs
+++ Assets/Gabriel/G_Scripts/Raycast_System.cs
@@ -3,9 +3,23 @@ using UnityEngine.UI;
 
 public class Raycast_System : MonoBehaviour
 {
-    public Text label;
-    float maxDist = 100f;
-    public LayerMask LayersToCheck;
+    // String "toolTip" is to be used as the string for the HUD tooltip the player gets when hovering over an object
+        public string toolTip;
+    
+    // Max distance for the raycast
+        float maxDist = 100f;
+
+        public LayerMask LayersToCheck;
+
+    [Header("Interact systems")]
+    // Interact to open
+        public Open_System Open;
+    // Interact to investigate
+        public Investigate_System Investigate;
+    // Interact to pick up
+        public Pickup_System Pickup;
+    // Interact using torch
+        public Ignite_System Ignite;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,24 +30,48 @@ public class Raycast_System : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            ActiveRay();
-        }
-            
+        ActiveRay();
+        Interact();
     }
 
-    private void ActiveRay()
+    public void ActiveRay()
     {
         Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit, maxDist, LayersToCheck))
         {
-            label.text = hit.collider.gameObject.name;
+            if (hit.collider.CompareTag("OBJ_Open"))
+            {
+                toolTip = "Open";
+            }
+            else if (hit.collider.CompareTag("OBJ_Investigate"))
+            {
+                toolTip = "Investigate";
+            }
+            else if (hit.collider.CompareTag("OBJ_Pickup"))
+            {
+                toolTip = "Pick up";
+            }
+            else if (hit.collider.CompareTag("OBJ_Ignite"))
+            {
+                toolTip = "Ignite";
+            }
         }
-        else
+    }
+
+    public void Interact()
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Low range");
+            if (toolTip == "Open")
+            {
+
+            }
         }
+    }
+
+    public void Trigger()
+    {
+
     }
 }
