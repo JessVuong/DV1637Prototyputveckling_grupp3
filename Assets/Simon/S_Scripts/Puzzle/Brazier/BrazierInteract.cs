382d8fffb8325bd75f282b7599266b1bd501ce00
Simon M. <heavenlylion37@gmail.com>
2026-05-04 13:44:53 +0200

    d

---
+++ Assets/Simon/S_Scripts/Puzzle/Brazier/BrazierInteract.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class BrazierInteract : MonoBehaviour, IInteractable
+{
+    public void Interact() 
+    { 
+        
+    
+    }
+    public string GetInteractionText()
+    {
+        return "Click to start puzzle...";
+    }
+}
---
+++ Assets/Simon/S_Scripts/Puzzle/Brazier/SequenceBrazier.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class SequenceBrazier : MonoBehaviour
+{ 
+    [Tooltip("Input for each of the Braziers")]
+    [SerializeField] private GameObject[] braziers;
+
+    void CheckSequence() 
+    { 
+        
+    }
+    
+}
